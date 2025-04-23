5cc5fd4ff5c9619e6e033361ef50e97e275d8702
iiojan <johan0905rdotmail.com>
2025-04-23 13:47:46 -0500

    Esenciales del consumo de la API

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import ramapi
+from flask import Flask, render_template 
 
-pejota = ramapi.Character.filter(name="Rick")
 
-for personaje in pejota["results"]:
-    print(personaje['name'], personaje['id'])
+app = Flask(__name__)
+
+@app.route("/")
+def personajes():
+
+    personajesPrincipales = ["Rick Sanchez", "Morty Smith", "Jerry Smith", "Summer Smith", "Beth Smith"]
+    personajesAMostrar = []
+
+    for pj in personajesPrincipales:
+        pejota = ramapi.Character.filter(name=pj)
+        personajes = pejota["results"]  # Lista de dicts
+        personajesAMostrar.extend(personajes)
+        
+    return render_template("index.html", personajes=personajesAMostrar)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
